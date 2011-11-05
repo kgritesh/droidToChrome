5bd69ce0d0e6889344e8e5b5951a15662fd483a4
Ritesh Kadmawala <k.g.ritesh@gmail.com>
2011-11-06 00:59:19 +0530

    Intermediate commit

--- app.js
+++ app.js
@@ -1,48 +1,81 @@
 var express = require('express'),
     cradle = require('cradle');
 
-var app = express.createServer(express.bodyParser());
-var conn = new(cradle.Connection)();
-var db = conn.database('users');
+var app = express.createServer();
+var conn = new(cradle.Connection)("http://localhost", 5984, {cache: true});
+var db_users = conn.database('users');
+var db_devices = conn.database('devices');
 var crypto = require('crypto');
+var io = require('socket.io').listen(app);
 
 app.use(express.logger());
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({ secret: "droidToChromeRMR" }));
 
 app.get('/', function(req, res){
     res.send("Hello World");
 });
 
 app.post('/register', function(req, res){
+    console.log(req.headers);
     var data = req.body;
     console.log(data);
-    db.get(data.username, function(err, doc) {
-    res.contentType('json');
+    res.contentType('application/json');
+    db_users.view('user/usermap', {key: data.username}, function(err, doc) {
     if(doc) {
 	console.log(data);
 	res.send({success: false, error: 'User already exists' });
     } else {
 	delete data.confirm_password;
 	data.password = converthash(data.password, randomString(10));
+	data.device_array = []
 	console.log(data.password)
-	db.save(data.username, data , function(db_err, db_res) {
+	db_users.save(data , function(db_users_err, db_users_res) {
+	    req.session.is_authenticated = true;
+	    req.session.user = data.usern;
 	    res.send({success:true});
       });
     }
   });
 });
 
-app.post('/login', function(req, res){
+app.post("/pc/login", function(req, res){
     var data = req.body;
     console.log(data);
-    res.contentType('json');
-    db.get(data.username, function(err, doc) {
-	if (doc){
+    res.contentType('application/json');
+    db_users.view('users/usermap', {key: data.username}, function(err, docs) {
+	if (docs){
+	    doc = docs[0].value;
 	    password = doc.password;
+	    console.log(doc);
 	    verify_password(data.password, password, function(match){
 		if(match){
-		    res.send({success:true});
+		    device_name = data.device_name;
+		    if(doc.device_array){
+			device_array = doc.device_array;
+		    }
+		    else{
+			device_array = [];
+		    }
+		    if(device_array.indexOf(device_name) == -1){
+			device_array.push(device_name);
+                        db_users.merge(doc._id, {"device_array": device_array},
+				function(err, res){
+				     console.log(err);
+                                     console.log(res);
+		        });
+			device_row = {"user_id": doc._id, "device": device_name}
+			//db_devices.save(device_row, pc_auth);
+
+                    }
+		    req.session.is_authenticated = true;
+		    req.session.username = data.username;
+		    res.send({"success":true, "uuid": doc._id});
+		    console.log("Success True");
 		}
 		else{
+		    req.session.is_authenticated = false;
 		    res.send({success: false, error: 'Password Didnt Match' });
 		}
 	    });
@@ -51,8 +84,48 @@ app.post('/login', function(req, res){
 	    res.send({success: false, error: "Username Doesn't Exist"});
 	}
     });
+
 });
 
+
+app.post('/login', function(req, res){
+    var data = req.body;
+    console.log(data);
+    res.contentType('json');
+    db_users.view('users/usermap', {key: data.username}, function(err, docs) {
+	if (docs){
+	    doc = docs[0].value;
+	    password = doc.password;
+	    verify_password(data.password, password, function(match){
+		if(match){
+		    req.session.is_authenticated = true;
+		    req.session.username = data.username;
+		    res.send({success:true});
+		}
+		else{
+		    req.session.is_authenticated = false;
+		    res.send({success: false, error: 'Password Didnt Match' });
+		}
+	    });
+	}
+	else{
+	    //req.sessions.is_authenticated = false
+	    res.send({success: false, error: "Username Doesn't Exist"});
+	}
+    });
+});
+
+
+app.post('/share', function(req, res){
+    //if(req.session.is_authenticated){
+        username = req.body.username;
+	link = req.body.url;
+	socket = sockets_hash[username];
+        socket.emit("urls", {"url" : link});
+    console.log(socket);
+        res.send({success:true});
+    //}
+});
 
 function verify_password(password, hash, callable){
     var salt = hash.split("$")[1];
@@ -78,6 +151,18 @@ function randomString(len){
      return randomStr;
 }
 
+sockets_hash = {}
 
+io.sockets.on('connection', function (socket) {
+  socket.on('auth', function (data) {
+      console.log(data);
+      uuid = data.uuid;
+      device_name = data.device_name;
+      db_users.get(uuid, function(err, doc){
+	  username = doc.username
+      });
+      console.log(data);
+  });
+});
 
-app.listen(3000);
+app.listen(8080);
