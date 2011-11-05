ce10a1a7b1f15b257d5e3e220786676af24927f6
Ritesh Kadmawala <k.g.ritesh@gmail.com>
2011-11-05 18:57:17 +0530

    Added code to hash password using sha1 and salt and also code to login user

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express'),
 var app = express.createServer(express.bodyParser());
 var conn = new(cradle.Connection)();
 var db = conn.database('users');
+var crypto = require('crypto');
 
 app.use(express.logger());
 
@@ -17,14 +18,66 @@ app.post('/register', function(req, res){
     db.get(data.username, function(err, doc) {
     res.contentType('json');
     if(doc) {
+	console.log(data);
 	res.send({success: false, error: 'User already exists' });
     } else {
-      delete data.confirm_password;
-      db.save(data.username, data, function(db_err, db_res) {
-	    res.send({success:True});
+	delete data.confirm_password;
+	data.password = converthash(data.password, randomString(10));
+	console.log(data.password)
+	db.save(data.username, data , function(db_err, db_res) {
+	    res.send({success:true});
       });
     }
   });
 });
+
+app.post('/login', function(req, res){
+    var data = req.body;
+    console.log(data);
+    res.contentType('json');
+    db.get(data.username, function(err, doc) {
+	if (doc){
+	    password = doc.password;
+	    verify_password(data.password, password, function(match){
+		if(match){
+		    res.send({success:true});
+		}
+		else{
+		    res.send({success: false, error: 'Password Didnt Match' });
+		}
+	    });
+	}
+	else{
+	    res.send({success: false, error: "Username Doesn't Exist"});
+	}
+    });
+});
+
+
+function verify_password(password, hash, callable){
+    var salt = hash.split("$")[1];
+    var hash_pass = converthash(password, salt);
+    if(hash_pass == hash)
+	callable(true)
+    else
+	callable(false)
+}
+
+function converthash(text, salt){
+    var hash = crypto.createHmac('sha1', salt).update(text).digest('hex');
+    return hash + "$" + salt;
+}
+
+function randomString(len){
+    var randomStr = ""
+    var charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < len; i++) {
+        var randomPoz = Math.floor(Math.random() * charSet.length);
+        randomStr += charSet.substring(randomPoz, randomPoz+1);
+    }
+     return randomStr;
+}
+
+
 
 app.listen(3000);
