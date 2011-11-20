69a8f772ecbd111487fe729e58e48c91122944cf
Ritesh Kadmawala <k.g.ritesh@gmail.com>
2011-11-20 18:03:41 +0530

    Added the entire chrome extensions. Needs lots of love and cleaning

---
+++ chrome-extension/js/socketcom.js
@@ -0,0 +1,27 @@
+function run()
+{     
+var bkg = chrome.extension.getBackgroundPage();
+try
+    {  
+      var uuid = window.localStorage.getItem('uuid');
+       if(uuid!=null)
+         {      
+       bkg.console.log(uuid);
+       var socket = io.connect('http://192.168.43.248:8080', { 'reconnect': true,
+       'reconnection delay': 500,
+     'max reconnection attempts': 10,
+      port: 8080 });
+     socket.emit('auth', {"uuid":uuid});
+    socket.on('urls', function (data) {
+    console.log(data);
+    chrome.tabs.create({'url': data.url}, function(tab) {
+        });
+     });
+     }
+  }
+    catch(e)
+     {
+     bkg.console.log(e);
+    }
+}
+
