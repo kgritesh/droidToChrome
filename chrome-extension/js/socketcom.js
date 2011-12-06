9ab5b0ca22fe9db540f7280bc8abed3cbbec2595
Ritesh Kadmawala <k.g.ritesh@gmail.com>
2011-12-06 18:17:10 +0530

    Changed the ip address

--- chrome-extension/js/socketcom.js
+++ chrome-extension/js/socketcom.js
@@ -1,27 +1,25 @@
-function run()
-{     
-var bkg = chrome.extension.getBackgroundPage();
-try
-    {  
+var ipaddress = "http://192.168.43.248:3000";
+function run(){
+  var bkg = chrome.extension.getBackgroundPage();
+  try
+    {
       var uuid = window.localStorage.getItem('uuid');
-       if(uuid!=null)
-         {      
-       bkg.console.log(uuid);
-       var socket = io.connect('http://192.168.43.248:8080', { 'reconnect': true,
-       'reconnection delay': 500,
-     'max reconnection attempts': 10,
-      port: 8080 });
-     socket.emit('auth', {"uuid":uuid});
-    socket.on('urls', function (data) {
-    console.log(data);
-    chrome.tabs.create({'url': data.url}, function(tab) {
-        });
-     });
+      if(uuid!=null){
+	bkg.console.log(uuid);
+	var socket = io.connect(ipaddress,
+			       {'reconnect': true,
+				'reconnection delay': 500,
+				'max reconnection attempts': 10,
+				port: 8080 });
+	socket.emit('auth', {"uuid":uuid});
+	socket.on('urls', function (data) {
+	  console.log(data);
+	  chrome.tabs.create({'url': data.url},function(tab) {});
+	});
      }
   }
-    catch(e)
-     {
-     bkg.console.log(e);
-    }
+  catch(e){
+    bkg.console.log(e);
+  }
 }
 
